4e0c1f15e18ecc1a34dbc5b539e36d50f1103664
Nelson Matsinhe <93186702+NelsonMatsinhe@users.noreply.github.com>
2025-10-06 16:57:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss()
+  ],
   base: '/portofolio/', // Deve corresponder ao nome do seu repositório
   resolve: {
     alias: {
