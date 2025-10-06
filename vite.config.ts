cdccdf2bab0e235bdf743dd626da4c8e9730f8f2
Nelson Matsinhe <93186702+NelsonMatsinhe@users.noreply.github.com>
2025-10-06 16:10:52 +0200

    configuracoes do build

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/portfolio/',
+  base: '/portofolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
